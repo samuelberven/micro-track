00d8fe9139a86815885be6ff659d23a5774320c2
sber-sber <samberven@gmail.com>
2023-09-20 21:11:29 -0700

    Update db-connector

--- database/db-connector.js
+++ database/db-connector.js
@@ -1,11 +1,3 @@
-/*
-    Citation for the following code:
-    Date: 2023/05/24
-    Adapted from:
-    https://github.com/osu-cs340-ecampus/nodejs-starter-app/tree/main/Step%201%20-%20Connecting%20to%20a%20MySQL%20Database
-*/
-
-// Get an instance of mysql we can use in the app
 var mysql = require('mysql')
 
 // Create a 'connection pool' using the provided credentials
@@ -18,5 +10,4 @@ var pool = mysql.createPool({
     database        : 'cs340_xyz'
 })
 
-// Export it for use in our application
 module.exports.pool = pool;
