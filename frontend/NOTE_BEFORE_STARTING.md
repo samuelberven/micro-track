ae25946bc167aab31ccd328c436826edbb7e0450
samuelberven <samberven@gmail.com>
2025-04-27 19:49:37 -0700

    Launch react.ts frontend, addd nodemon, ts-node, and tailwind (NOTE: tailwind font colors are not working-- needs debugging)

--- frontend/NOTE_BEFORE_STARTING.md
+++
@@ -1,7 +0,0 @@
-Add this to ./frontend/package.json after react app created:
-
-"scripts": {
-"lint": "eslint . --ext .tsx,.ts",
-"lint:fix": "eslint . --ext .tsx,.ts --fix",
-"prettier:fix": "npx prettier --write ."
-}
---
+++ frontend/vite.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// Allows for Docker usage (and hot reloading ultimately, I believe)
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    host: '0.0.0.0',  // Important for Docker
+    port: 5173,
+    watch: {
+      usePolling: true  // Better for Docker
+    }
+  }
+})
